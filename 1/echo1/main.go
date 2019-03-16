face54afa68e243a98ae70e98c36754e5502874e
iliuxu <imliuxu@gmail.com>
2019-03-16 22:07:15 +0800

    echo

---
+++ 1/echo1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// 输出命令行参数
+func main()  {
+	var s, sep string
+	for i :=1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
